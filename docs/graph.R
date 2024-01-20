c1e713ca3abac02655e1fef4df662aa98b52a1b8
kucukkoring <155237438+kucukkoring@users.noreply.github.com>
2024-01-21 01:42:59 +0300

    graph.R

--- docs/graph.R
+++ docs/graph.R
@@ -3,29 +3,33 @@ source('https://raw.githubusercontent.com/kucukkoring/413P/main/docs/evds.R')
 
 ## Required Packages
 if (!require(plotly)) install.packages('plotly')
+library(plotly)
 ## Graph
 
+graph1 <- ggplotly(
+  ggplot(merged, aes(x=Housing_Interest_Rate, y=Ankara_First_Hand_House_Sales)) +
+    geom_point(color = "purple") +
+    geom_smooth(formula = y ~ x, method = "lm", se = TRUE, color = "blue") +
+    theme_minimal() +
+    labs(title = "Ankara First Hand House Sales & Housing Interest Rate", x = "Ankara House Price Index", y = "Ankara First Hand House Sales") +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+)
+graph1
 
-fig <- plot_ly(kfe, x = ~Tarih, y = ~İstanbul, type = 'scatter', mode = 'lines')
+graph2 <- ggplotly(
+  ggplot(merged, aes(x=Ankara_House_Price_Index, y=Ankara_First_Hand_House_Sales)) +
+    geom_point(color = "purple") +
+    geom_smooth(formula = y ~ x, method = "lm", se = TRUE, color = "blue") +
+    theme_minimal() +
+    labs(title = "Ankara First Hand House Sales & Ankara House Price Index", x = "Ankara House Price Index", y = "Ankara First Hand House Sales") +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+)
+graph2
 
-fig2 <- plot_ly(konut_faiz, x = ~Tarih, y = ~XD, type = 'scatter', mode = 'lines')
-
-graph_1 <- plot_ly(kfe, x = ~Tarih, y = ~İstanbul , type = 'scatter' , mode = 'lines')
-graph_1
-
-graph_2 <- plot_ly(konut_faiz, x = ~Tarih, y = ~XD, type = 'scatter', mode = 'lines')
-graph_2
 
 
-graph_3 <- plot_ly(kfe, x = ~Tarih, y = ~Ankara , type = 'scatter' , mode = 'lines')
-graph_3
-
-graph_1 <- plot_ly(kfe, x = ~Tarih, y = ~İstanbul, type = 'scatter', mode = 'lines', name = 'İstanbul') %>%
-  add_trace(x = ~Tarih, y = ~Ankara, type = 'scatter', mode = 'lines', name = 'Ankara')
-
-graph_1
-
-fig
 
 
 
